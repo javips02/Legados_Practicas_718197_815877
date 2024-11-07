e270d8fa7e4c1f89afef1bd6c1da6b75933ea6de
Javier <119667872+javips02@users.noreply.github.com>
2024-11-07 12:18:31 +0100

    Creado script de empaquetado de exe (segundo intento sigue mal v2)

--- pr3/DataBaseWrapperPy_Grupo09/empaquetarEXE.bat
+++ pr3/DataBaseWrapperPy_Grupo09/empaquetarEXE.bat
@@ -1,1 +1,11 @@
-pyinstaller --onefile --windowed --add-data "Database-MSDOS;Database-MSDOS" --add-data "tesseract;tesseract" --add-data "templates;templates" --hidden-import "pytesseract" wrapper.py
+pyinstaller --onefile --windowed --exclude-module tkinter `
+            --add-data "Database-MSDOS;Database-MSDOS" `
+            --add-data "tesseract;tesseract" `
+            --add-data "templates;templates" `
+            --hidden-import "pytesseract" `
+            --hidden-import "PIL._imaging" `
+            --hidden-import "PIL._imagingtk" `
+            --hidden-import "PIL.Image" `
+            --hidden-import "difflib" `
+            --hidden-import "flask" `
+            app.py
