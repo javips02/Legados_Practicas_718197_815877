abb3c5dc5bf1ce17325dba499017f9bdb3191963
Javier <119667872+javips02@users.noreply.github.com>
2024-11-11 12:21:40 +0100

    Create ejecutar.bat

---
+++ pr3/DataBaseWrapperPy_Grupo09/ejecutar.bat
@@ -0,0 +1,50 @@
+@echo off
+setlocal
+
+:: Configuración
+set PYTHON_VERSION=3.11.0
+set PYTHON_INSTALLER=python-%PYTHON_VERSION%-amd64.exe
+set PYTHON_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/%PYTHON_INSTALLER%
+
+:: Descargar Python si no está instalado
+echo Comprobando si python esta instalado ...
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python no está instalado. Descargando Python %PYTHON_VERSION%...
+    curl -o %PYTHON_INSTALLER% %PYTHON_URL%
+    
+    echo Instalando Python %PYTHON_VERSION%...
+    start /wait %PYTHON_INSTALLER% /quiet PrependPath=1 Include_test=0
+
+    :: Verificar si Python se instaló correctamente
+    python --version >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo Error al instalar Python. Abortando.
+        pause
+        exit /b 1
+    )
+)
+
+set REQ_PATH=.\practica3
+:: Ruta donde se creará el entorno virtual
+set VENV_PATH=.\env
+:: Ruta al pip del entorno virtual
+set PIP="%VENV_PATH%\Scripts\pip.exe"
+
+:: Crear el entorno virtual
+echo Creando entorno virtual en %VENV_PATH% ...
+python -m venv "%VENV_PATH%"
+
+:: Activar el entorno virtual
+call "%VENV_PATH%\Scripts\activate.bat"
+
+:: Instalar las dependencias de tu aplicación 
+echo Instalando dependencias ...
+%PIP% install -r "dependencias.txt"  > nul 2>&1
+
+:: Ejecutar la aplicación Flask y esperar a que termine 
+cd app
+echo Lanzando aplicación ...
+python main.py
+pause
+endlocal
